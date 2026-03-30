85552808fc7d08fb30e83addc37c0f0171d0bb40
waleed <merabetwalid15@gmail.com>
2026-03-30 18:04:58 +0100

    fix int problem

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -141,7 +141,7 @@ MEDIA_URL = '/media/'
 # email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_PORT = env.int('EMAIL_PORT')
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
